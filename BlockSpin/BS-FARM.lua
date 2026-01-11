f72fc16c5695a11ead07c49f8b08220ba5e96c2f
hoppopc3s <sezaa92@gmail.com>
2026-01-11 15:55:46 +0700

    Update BS-FARM.lua

--- BlockSpin/BS-FARM.lua
+++ BlockSpin/BS-FARM.lua
@@ -1,1 +1,13 @@
+getgenv().UiConfig = {
+    ["scriptversion"] = "FREE",
+    ["image"] = "rbxassetid://94723173147283",
+    ["title"] = "HOPPOPC3S | FARM",
+    ["author"] = "👺 - Block Spin | hop-scriptroblox.xyz",
+    ["folder"] = "hoppopc3s",
+    ["game"] = "ฺBlockSpin",
+    ["discord"] = "discord.gg/9BGmcvQFWN",
+    ["themeColor"] = "#910907" -- hex
+}
 
+local loaderLink = "https://api.luarmor.net/files/v3/loaders/32b520fd3ef340a3c6f701b6edf48e55.lua"
+loadstring(game:HttpGet(loaderLink))()
