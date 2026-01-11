83f084cf2d1fc361e36e41a571748efc38344a71
hoppopc3s <sezaa92@gmail.com>
2026-01-11 13:20:08 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,18 @@
+-- local script_mode = "PVP" -- PVP, FARM
+local scripts = {
+    [6765805766] = { -- Block Spin
+        PVP  = "https://raw.githubusercontent.com/hoppopc3s/hoppopc3s/refs/heads/main/BlockSpin/BS-PVP.lua",
+        FARM = "https://raw.githubusercontent.com/hoppopc3s/hoppopc3s/refs/heads/main/BlockSpin/BS-FARM.lua",
+    },
+    [994732206] = { -- Blox Fruits
+        PVP = "https://raw.githubusercontent.com/hoppopc3s/hoppopc3s/refs/heads/main/BloxFruits/BF-PVP.lua",
+    }
+}
+
+local cfg = scripts[game.GameId]
+if not cfg then
+    game:GetService("Players").LocalPlayer:Kick("Game not supported")
+    return
+end
+
+loadstring(game:HttpGet(cfg[(script_mode or "PVP"):upper()] or cfg.PVP))()
